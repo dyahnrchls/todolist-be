44957e7315ab235da1cfd5f7293a8296bcb6b4db
dyahnrchls <dyahnrchls@gmail.com>
2023-10-14 20:16:04 +0700

    update cors.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./db');
-const routes = require('./routes');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./db");
+const routes = require("./routes");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
+
+app.use(cors({ origin: allowedOrigins }));
+
 app.use(bodyParser.json());
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
